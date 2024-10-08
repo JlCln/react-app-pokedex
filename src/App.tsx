ea8c5e548cbd9ce46a17bb199e7d1e8e9cc3c2d3
JlCln <j.coulon.pro@gmail.com>
2024-10-08 15:32:13 +0200

    using props on props branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ import PokemonCard from "./components/PokemonCard";
 function App() {
   return (
     <div className="App">
-      <PokemonCard/>
+      <PokemonCard pokemon={pokemonList[0]}/>
+      <p style={{ color: "#ff9500", fontWeight: "bold", textDecoration: "underline" }}>Using props</p>
+      <PokemonCard pokemon={pokemonList[1]}/>
       </div>
   );
 }
+
+const pokemonList = [
+  {
+    name: "#0001: Bulbasaur / Bulbizarre / フシギダネ",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "#0151: Mew / Mew / ミュウ",
+  },
+];
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,6 +1,4 @@
-function PokemonCard() {
-    let pokemon = pokemonList[0];
-    let pokemonOther = pokemonList[1];
+function PokemonCard({ pokemon }) {
     return (
       <figure>
         {pokemon.imgSrc ? (
@@ -9,29 +7,10 @@ function PokemonCard() {
           <p>???</p>
         )}
         <figcaption>{pokemon.name}</figcaption>
-        <p style={{ color: "red", fontWeight: "bold", textDecoration: "underline" }}>Other Pokemon</p>
-        <figure>
-        {pokemonOther.imgSrc ? (
-          <img src={pokemonOther.imgSrc} />
-        ) : (
-          <p>???</p>
-        )}
-        <figcaption>{pokemonOther.name}</figcaption>
-      </figure>
       </figure>
     );
     
   }
 
-const pokemonList = [
-    {
-      name: "#0001: Bulbasaur / Bulbizarre / フシギダネ",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "#0151: Mew / Mew / ミュウ",
-    },
-  ];
 
 export default PokemonCard;
