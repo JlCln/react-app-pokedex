9aec325b879266c1cdd6680c328a6b013d290dd1
JlCln <j.coulon.pro@gmail.com>
2024-10-08 13:50:06 +0200

    modified Pokemon Card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,37 @@
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"/>
-    <figcaption>Bulbasaur / Bulbizarre / フシギダネ</figcaption>
-    </figure>;
+    let pokemon = pokemonList[0];
+    let pokemonOther = pokemonList[1];
+    return (
+      <figure>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+        <p style={{ color: "red", fontWeight: "bold", textDecoration: "underline" }}>Other Pokemon</p>
+        <figure>
+        {pokemonOther.imgSrc ? (
+          <img src={pokemonOther.imgSrc} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemonOther.name}</figcaption>
+      </figure>
+      </figure>
+    );
     
-}
+  }
+
+const pokemonList = [
+    {
+      name: "#0001: Bulbasaur / Bulbizarre / フシギダネ",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "#0151: Mew / Mew / ミュウ",
+    },
+  ];
 
 export default PokemonCard;
